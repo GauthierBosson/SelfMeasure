86e7b27413d390d421a64e25bbe6568aae109d2b
Gauthier <16987@csmrouen.net>
2019-02-22 09:57:47 +0100

    Ajout page profil pour ajout de poids

--- src/app/historic/historic.page.ts
+++ src/app/historic/historic.page.ts
@@ -16,7 +16,6 @@ export class HistoricPage implements OnInit {
   @ViewChild('chartCanvas2') chartCanvas2;
 
   barChart: any;
-  barChart2: any;
   weight: any;
   date: any;
   values: any[];
@@ -67,44 +66,6 @@ export class HistoricPage implements OnInit {
         datasets: [{
           label: 'Poids en kg',
           data: weights,
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
-          ],
-          borderColor: [
-            'rgba(255,99,132,1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
-          ],
-          borderWidth: 1
-        }]
-      },
-      options: {
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero: true
-            }
-          }]
-        }
-      }
-    });
-
-    this.barChart2 = new Chart(this.chartCanvas2.nativeElement, {
-
-      type: 'pie',
-      data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-          label: '# of Votes',
-          data: [3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
           backgroundColor: [
             'rgba(255, 99, 132, 0.2)',
             'rgba(54, 162, 235, 0.2)',
